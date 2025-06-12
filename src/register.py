578f8d1666b015ca056a081b8978d0e3e1c43c4f
thomas-is <thomas@0l.fi>
2025-06-12 16:55:06 +0200

    🐍 allow multiple arguments, improve timing and logs

--- src/register.py
+++ src/register.py
@@ -7,10 +7,16 @@ import sys
 from datetime import datetime
 from playwright.sync_api import sync_playwright
 
-day     = sys.argv[1]
-on_site = ( sys.argv[2] == "office" )
-
 VIDEO_DIR = "/home/python/videos/"
+
+class WorkingDay:
+    ymd    = "today"
+    status = True      # at_office?
+    def __init__(self, when, status):
+        self.ymd = dateparser.parse(when).strftime("%Y-%m-%d")
+        self.status = ( status.lower() == "office" )
+    def __repr__(self):
+        return f"{self.ymd} office" if self.status else f"{self.ymd} remote"
 
 def msg(text):
     print(f"{text}", flush=True)
@@ -23,14 +29,11 @@ def get_today_selector():
     return get_selector(today)
 
 def login(page):
-
-    url        = os.getenv("LOGIN_URL")
-    username   = os.getenv("USERNAME")
-    password   = os.getenv("PASSWORD")
-
-    page.goto(url)
-
+    url      = os.getenv("LOGIN_URL")
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
     msg(f"👨 login")
+    page.goto(url)
     user_field = page.locator('input#id.champConnexion')
     user_field.fill(username)
     pw_field = page.locator('input#pw.champConnexion')
@@ -51,57 +54,53 @@ def working_time_open(page):
     e = page.locator(selector)
     e.click()
     msg(f"⌛ wait for the planning to be loaded")
-    page.wait_for_timeout(5000)
-
-def working_time_send(page):
-    msg(f"✉️  send")
-    e = page.get_by_text('Envoyer les demandes')
-    e.click()
     page.wait_for_timeout(3000)
 
-
 def working_time_set(page, day="today", on_site=False):
-
     ymd = dateparser.parse(day).strftime("%Y-%m-%d")
-
     status = "office" if on_site else "remote"
-
     msg(f'⚙️  set {ymd} as {status}')
     # this is the manager cell for a certain date
     # the cell just below is ours
     selector = get_selector(ymd)
-    page.wait_for_timeout(5000)
-    msg(f"  ℹ️  {selector}")
+    page.wait_for_timeout(500)
+    msg(f"   ℹ️  {selector}")
     e = page.locator(selector).bounding_box()
     x = e['x'] + e['width'] / 2    # center on manager cell
     y = e['y'] + e['height'] * 1.5 # center on manager cell and go below one row
-
     page.mouse.click(x, y, button='right')
-    page.wait_for_timeout(1000)
+    page.wait_for_timeout(500)
     page.keyboard.press("ArrowDown")
     page.keyboard.press("ArrowDown")
     if on_site:
         page.keyboard.press("ArrowDown")
     page.keyboard.press("Enter")
-
     selector='input[placeholder="Motif"]'
-    msg(f"  ℹ️  {selector}")
-    e = page.locator(selector)
-    e.click()
-    page.wait_for_timeout(1000)
-    page.keyboard.press("ArrowDown")
-    page.keyboard.press("Enter")
-    page.wait_for_timeout(1000)
+    msg(f"   ℹ️  {selector}")
+    try:
+        e = page.locator(selector)
+        e.click(timeout=500)
+        #page.wait_for_timeout(500)
+        page.keyboard.press("ArrowDown")
+        page.keyboard.press("Enter")
+        page.wait_for_timeout(500)
+        selector = 'button[title="Ajouter"]'
+        msg(f"   ℹ️  {selector}")
+        e = page.locator(selector)
+        e.click(timeout=500)
+        page.wait_for_timeout(500)
+    except Exception as e:
+        msg(f"   💣 an error occurred")
 
-    selector = 'button[title="Ajouter"]'
-    msg(f"  ℹ️  {selector}")
-    e = page.locator(selector)
-    e.click()
-    page.wait_for_timeout(2000)
+def working_time_send(page):
+    msg(f"✉️  send")
+    e = page.get_by_text('Envoyer les demandes')
+    e.click(timeout=500)
+    msg(f"⌛ wait a bit")
+    page.wait_for_timeout(5000)
 
 
-def run(playwright, day="today", on_site=False):
-
+def run(playwright, working_days):
     user_agent = os.getenv("USER_AGENT")
     chromium = playwright.chromium
     browser = chromium.launch(headless=True)
@@ -111,17 +110,26 @@ def run(playwright, day="today", on_site=False):
         user_agent=user_agent
     )
     page = context.new_page()
-
     login(page)
     working_time_open(page)
-    working_time_set(page, day=day, on_site=on_site)
+    for d in working_days:
+        working_time_set(page, day=d.ymd, on_site=d.status)
     working_time_send(page)
-
     context.close()
     browser.close()
 
 
+# main
+
+working_days = []
+for n in range(1, len(sys.argv), 2):
+    working_day = WorkingDay(sys.argv[n], sys.argv[n+1])
+    working_days.append(working_day)
+msg(f"ℹ️  tasks:")
+for d in working_days:
+    msg(f"   register {d}")
+
 with sync_playwright() as playwright:
     msg("🚀 launch playwright")
-    run(playwright, day=day, on_site=on_site)
+    run(playwright, working_days)
 
